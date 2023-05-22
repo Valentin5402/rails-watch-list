d11850bb2d309a52d17e4b5346ead4dd563e0da8
Valentin MEROU <valentin.merou@gmail.com>
2023-05-22 14:44:09 +0200

    application fonctionnelle

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "open-uri"
+require "json"
+
+Movie.destroy_all
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+top_rated_films = JSON.parse(URI.open(url).read)
+
+top_rated_films["results"].each do |film|
+  Movie.create!(
+    title: film["title"],
+    overview: film["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{film['poster_path']}",
+    rating: film["vote_average"]
+  )
+end
